7c5446e360638e18aa13f7fed460388eb0131740
Sujeet17m <sujeet17das@gmail.com>
2025-10-04 02:13:27 +0530

    updated

--- notes-app/src/pages/Home/index.jsx
+++ notes-app/src/pages/Home/index.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
-import Navbar from '../../components/Navbar';
-import { Sidebar } from '../../components/Sidebar';
+import React from "react";
+import Navbar from "../../components/Navbar";
+import { Sidebar } from "../../components/Sidebar";
+import { useState } from "react";
 
-export default function Home() {
+export const Home = () => {
+  const [text, setText] = useState("");
+  const [title, setTitle] = useState("");
   return (
     <div>
       <Navbar />
-      <main>
+      <main className="flex gap-3">
         <Sidebar />
+        <div>
+          <div className="flex flex-col w-[400px] border-red-400 relative">
+            <input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="border border-gray-300 rounded-md"
+              placeholder="Note title"
+            />
+
+            <textarea
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              className="border border-gray-300 rounded-md"
+              placeholder="Write your note here..."
+            ></textarea>
+            <button className="absolute bottom-0 right-0">
+              <span className="material-symbols-outlined">add</span>
+            </button>
+          </div>
+        </div>
       </main>
     </div>
   );
-}
-
-
+};
